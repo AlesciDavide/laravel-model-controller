4eb6f200f93f47a26ee25ed30fd942969b38bcda
AlesciDavide <davi.alesci@Gmail.com>
2024-07-17 14:09:33 +0200

    add css

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,6 @@ import './bootstrap';
 import '~resources/scss/app.scss';
 
 
-
-
 import.meta.glob([
     '../img/**'
 ])
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layout.app')
 @section('page-title', 'Movie List')
-
+@section('head-cdn')
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+@endsection
 
 
 
@@ -8,8 +11,8 @@
     @section('main-content')
 
 
-    @foreach ($movies as $movie)
-        <section class="container">
+    <section class="container">
+        @foreach ($movies as $movie)
             <article class="singleFilm">
                 <div>
                     <h1>{{ $movie['title']}}</h1>
@@ -19,8 +22,8 @@
                     <p>{{ $movie['vote']}}</p>
                 </div>
             </article>
+            @endforeach
         </section>
-    @endforeach
 
 
     @endsection
